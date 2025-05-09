7faaea5608dc58f98c9eb688f39a901c7654709c
MiguelTalamantez <miguelantoniotalamantez@gmail.com>
2025-05-09 03:04:03 -0400

    add check at top

--- V2/main_dev_presentation.py
+++ V2/main_dev_presentation.py
@@ -6,6 +6,10 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import RPi.GPIO as GPIO
+if GPIO.getmode() is None:
+    GPIO.setmode(GPIO.BOARD)
+elif GPIO.getmode() != GPIO.BOARD:
+    raise RuntimeError("GPIO mode already set to a different mode!")
 import threading
 import time
 import board
