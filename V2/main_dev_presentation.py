38cc0cd2fd536bc05b93b9c8763a6fd187a81b28
MiguelTalamantez <miguelantoniotalamantez@gmail.com>
2025-05-09 07:32:14 -0400

    christian change controller

--- V2/main_dev_presentation.py
+++ V2/main_dev_presentation.py
@@ -474,13 +474,15 @@ class pHFrame(ParameterFrame):
                         short_moving_avg = sum(self._raw_to_ph(v) for v in self.live_voltage[-5:]) / 5 if self.live_voltage else None
                         error = setpoint-short_moving_avg
                         error_sum += error
-                        response = 0.005 * 1/( abs(error) + 0.1 )
+                        response = round(0.005 * 1/( abs(error) + 0.1 ),4)
                         print("error")
                         print(error)
                         print("response")
                         print(response)
+                    
                         if short_moving_avg is not None and setpoint is not None and error < 0:
                             if pump_controller:
+                                print("pumping")
                                 pump_controller.pump_action(1, 1, steps=800, speed=response)
                             else:
                                 break
